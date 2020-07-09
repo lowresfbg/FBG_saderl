Correct the formula

Dataset/Simulation/GaussCurve.py
@@ -4,7 +4,7 @@ from numba import jit
 
 @jit
 def GaussCurve(x, I, C, W):
-    return I*np.exp(-np.sqrt(2)*4*((x-C)/W)**2)
+    return I*np.exp(-np.log(2)*4*((x-C)/W)**2)
 
 
 @jit

Dataset/Simulation/GaussCurve_TF.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @tf.function
 def GaussCurve(x, I, C, W):
-    return I*tf.exp(-tf.sqrt(2.0)*4*((x-C)/W)**2)
+    return I*tf.exp(-tf.math.log(2.0)*4*((x-C)/W)**2)
 
 @tf.function
 def FBG_spectra(x_coord, X, I, W):
@@ -15,5 +15,5 @@ def FBG_spectra(x_coord, X, I, W):
 
     return tf.reduce_sum(GaussCurve(x_coord, I, X, W), axis=1)
 
-
-
+def GaussCurve_Graph(x, I, C, W):
+    return I*tf.exp(-tf.sqrt(2.0)*4*((x-C)/W)**2)