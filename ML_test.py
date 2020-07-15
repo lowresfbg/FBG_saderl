7b8776eb60f6c4cb8a220eafd066ebf3fad6a189
Hans Chiu <chiuhans111@gmail.com>
2020-07-15 12:12:52 +0800

    train model

--- ML_test.py
+++ ML_test.py
@@ -16,12 +16,12 @@ x_coord = tf.linspace(0.0, 1.0, 1000)
 
 X1 = tf.random.uniform([samples, fbgs])
 I1 = tf.random.uniform([samples, fbgs], 0.1, 1)
-W1 = tf.random.uniform([samples, fbgs], 0.1, 0.3)
+W1 = tf.random.uniform([samples, fbgs], 0.01, 0.2)
 spectrums1 = normalize(FBG_spectra(x_coord, X1, I1, W1))
 
 X2 = tf.random.uniform([samples, fbgs])
 I2 = tf.random.uniform([samples, fbgs], 0.1, 1)
-W2 = tf.random.uniform([samples, fbgs], 0.1, 0.3)
+W2 = tf.random.uniform([samples, fbgs], 0.01, 0.2)
 spectrums2 = normalize(FBG_spectra(x_coord, X2, I1, W2))
 
 train_X = tf.concat([tf.expand_dims(spectrums1, axis=1),
@@ -34,7 +34,7 @@ train_Y = tf.reduce_mean(tf.abs(X2-X1), axis=1)
 # plt.show()
 
 e_model.summary()
-# e_model.load_weights('./SavedModel/SignalErrorModel.hdf5')
+e_model.load_weights('./SavedModel/SignalErrorModel.hdf5')
 
 e_model.compile(optimizer=tf.keras.optimizers.Adam(lr=1e-3), loss="mse")
 
