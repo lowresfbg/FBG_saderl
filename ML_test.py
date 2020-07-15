823495ffa12d293998f343ff2a3c249e6e7b6685
Hans Chiu <chiuhans111@gmail.com>
2020-07-15 16:21:19 +0800

    update training

--- ML_test.py
+++ ML_test.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 e_model, f_model = SignalError.ErrorModel()
 
-samples = 10000
+samples = 100000
 fbgs = 3
 
 
@@ -18,14 +18,14 @@ x_coord = tf.linspace(0.0, 1.0, 1000)
 
 X1 = tf.random.uniform([samples, fbgs])
 I1 = tf.random.uniform([samples, fbgs], 0.1, 1)
-W1 = tf.ones([samples, fbgs]) * tf.random.uniform([1], 0.01, 0.2)
+W1 = tf.ones([samples, fbgs]) * tf.random.uniform([1], 0.01, 0.1)
 spectrums1 = normalize(FBG_spectra(x_coord, X1, I1, W1))
 
 X2 = tf.random.uniform([samples, fbgs])
-I2 = tf.random.uniform([samples, fbgs], 0.1, 1)
-W2 = W1 + tf.random.uniform([samples, fbgs])*0.005
+
+W2 = W1 + (tf.random.uniform([samples, fbgs])-0.5)*0.005
 spectrums2 = normalize(FBG_spectra(x_coord, X2, I1, W2) +
-                       tf.random.uniform([samples, 1000])*1e-5)
+                       (tf.random.uniform([samples, 1000])-0.5)*1e-5)
 
 train_X = tf.concat([tf.expand_dims(spectrums1, axis=1),
                      tf.expand_dims(spectrums2, axis=1)], axis=1)
@@ -39,11 +39,11 @@ plt.show()
 e_model.summary()
 e_model.load_weights('./SavedModel/SignalErrorModel.hdf5')
 
-e_model.compile(optimizer=tf.keras.optimizers.Adam(lr=1e-3), loss="mse")
+e_model.compile(optimizer=tf.keras.optimizers.Adam(lr=1e-2), loss="mse")
 
-for i in range(100):
+for i in range(10):
     print("training cycle", i)
-    e_model.fit(train_X, train_Y, epochs=10, batch_size=1000, shuffle=True)
+    e_model.fit(train_X, train_Y, epochs=10, batch_size=5000, shuffle=True)
     e_model.save_weights('./SavedModel/SignalErrorModel.hdf5')
 
 pred_Y = e_model(train_X)[:, 0]
