6d3a7047681654d844ae9fecd477fddf9a0246db
Hans Chiu <chiuhans111@gmail.com>
2020-07-15 12:07:59 +0800

    new model

--- ML_test.py
+++ ML_test.py
@@ -34,7 +34,7 @@ train_Y = tf.reduce_mean(tf.abs(X2-X1), axis=1)
 # plt.show()
 
 e_model.summary()
-e_model.load_weights('./SavedModel/SignalErrorModel.hdf5')
+# e_model.load_weights('./SavedModel/SignalErrorModel.hdf5')
 
 e_model.compile(optimizer=tf.keras.optimizers.Adam(lr=1e-3), loss="mse")
 
@@ -42,6 +42,7 @@ for i in range(100):
     print("training cycle", i)
     e_model.fit(train_X, train_Y, epochs = 10, batch_size=1000, shuffle=True)
     e_model.save_weights('./SavedModel/SignalErrorModel.hdf5')
+
 pred_Y = e_model(train_X)[:,0]
 print(pred_Y.shape, train_Y.shape)
 plt.plot(pred_Y-train_Y, "o")
