76df5f8b2adff175d3c68cec7e90ba6e2ba041e2
Hans Chiu <chiuhans111@gmail.com>
2020-07-15 16:27:26 +0800

    new model

--- Model/SignalError.py
+++ Model/SignalError.py
@@ -4,11 +4,11 @@ import tensorflow as tf
 def FeatureModel():
     spectra_input = tf.keras.Input((1000,))
     x = tf.expand_dims(spectra_input, axis=2)
-    x = tf.keras.layers.Conv1D(20, 10, activation='sigmoid')(x)
+    x = tf.keras.layers.Conv1D(50, 10, activation='sigmoid')(x)
     x = tf.keras.layers.MaxPooling1D(2)(x)
-    x = tf.keras.layers.Conv1D(10, 5, activation='sigmoid')(x)
+    x = tf.keras.layers.Conv1D(20, 5, activation='sigmoid')(x)
     x = tf.keras.layers.MaxPooling1D(2)(x)
-    x = tf.keras.layers.Conv1D(5, 3, activation='sigmoid')(x)
+    x = tf.keras.layers.Conv1D(10, 3, activation='sigmoid')(x)
     x = tf.keras.layers.MaxPooling1D(2)(x)
     x = tf.keras.layers.Flatten()(x)
     x = tf.keras.layers.Dense(10, activation='sigmoid')(x)
@@ -21,7 +21,7 @@ def ErrorModel():
     feature1 = feature_model(spectra_input[:,0])
     feature2 = feature_model(spectra_input[:,1])
     x = tf.abs(feature1-feature2)
-    # x = tf.keras.layers.Dense(10, activation='sigmoid')(x)
+    x = tf.keras.layers.Dense(10, activation='sigmoid')(x)
     x = tf.keras.layers.Dense(1)(x)
 
     return tf.keras.Model(spectra_input, x), feature_model
