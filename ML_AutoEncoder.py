616d963185bc2531ce686532eb243f042939a0b6
Hans Chiu <chiuhans111@gmail.com>
2020-07-20 15:45:49 +0800

    auto encoder model

---
+++ ML_AutoEncoder.py
@@ -0,0 +1,49 @@
+from Model import AutoEncoderWLCNN
+from Dataset.Simulation.GaussCurve_TF import FBG_spectra
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+fbgs = 5
+samples = 2000
+
+encdec, model = AutoEncoderWLCNN.GetModel(5)
+
+
+
+def normalize(spectra):
+    maximum = tf.expand_dims(tf.reduce_max(spectra, axis=1), axis=1)
+    minimum = tf.expand_dims(tf.reduce_min(spectra, axis=1), axis=1)
+    return (spectra-minimum)/(maximum-minimum)
+
+
+x_coord = tf.linspace(0.0, 1.0, 1000)
+
+X1 = tf.random.uniform([samples, fbgs])
+I1 = tf.random.uniform([samples, fbgs])
+W1 = tf.ones([samples, fbgs]) * tf.random.uniform([1], 0.05, 0.15)
+spectrums1 = normalize(FBG_spectra(x_coord, X1, I1, W1))
+
+
+train_X = spectrums1
+
+train_Y = spectrums1
+
+plt.plot(spectrums1[0])
+plt.show()
+
+encdec.summary()
+encdec.load_weights('./SavedModel/EncDecModel.hdf5')
+
+encdec.compile(optimizer=tf.keras.optimizers.Adam(lr=1e-2), loss="mse")
+
+for i in range(10):
+    print("training cycle", i)
+    encdec.fit(train_X, train_Y, epochs=10, batch_size=2000, validation_split=0.2, shuffle=True)
+    encdec.save_weights('./SavedModel/EncDecModel.hdf5')
+
+pred_Y = encdec(train_X)
+print(pred_Y.shape, train_Y.shape)
+for i in range(samples):
+    plt.plot(pred_Y[i], "o")
+    plt.plot(train_Y[i], "-")
+    plt.show()
--- Model/AutoEncoderWLCNN.py
+++ Model/AutoEncoderWLCNN.py
@@ -3,29 +3,52 @@ import tensorflow as tf
 def Encoder(fbg_count):
     spectra_input = tf.keras.Input((1000,))
     x = tf.expand_dims(spectra_input, axis=2)
-    x = tf.keras.layers.Conv1D(fbg_count*20, 3, activation='relu')(x)
+    x = tf.keras.layers.Conv1D(fbg_count*20, 3, activation='relu', padding='same')(x)
     x = tf.keras.layers.MaxPooling1D(2)(x)
-    x = tf.keras.layers.Conv1D(fbg_count*20, 5, activation='relu')(x)
+    x = tf.keras.layers.Conv1D(fbg_count*20, 5, activation='relu', padding='same')(x)
     x = tf.keras.layers.MaxPooling1D(2)(x)
-    x = tf.keras.layers.Conv1D(fbg_count*20, 5, activation='relu')(x)
+    x = tf.keras.layers.Conv1D(fbg_count*5, 5, activation='relu', padding='same')(x)
     x = tf.keras.layers.MaxPooling1D(2)(x)
-    x = tf.keras.layers.Dense(fbg_count*5,activation='sigmoid')(x)
+    x = tf.keras.layers.Permute((2,1))(x)
     x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(1))(x)
+    x = tf.keras.layers.Flatten()(x)
     return tf.keras.Model(spectra_input, x)
+
+def Decoder(fbg_count):
+    represent_input = tf.keras.Input((fbg_count*5,))
+    x = tf.expand_dims(represent_input, axis=2)
+    x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(125))(x)
+    x = tf.keras.layers.Permute((2,1))(x)
+
+    x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(125))(x)
+
+    x = tf.keras.layers.UpSampling1D(2)(x)
+    x = tf.keras.layers.Conv1D(fbg_count*20, 5, activation='relu', padding='same')(x)
+    x = tf.keras.layers.UpSampling1D(2)(x)
+    x = tf.keras.layers.Conv1D(fbg_count*20, 5, activation='relu', padding='same')(x)
+    x = tf.keras.layers.UpSampling1D(2)(x)
+    x = tf.keras.layers.Conv1D(1, 3, activation='relu', padding='same')(x)
+    x = tf.keras.layers.Flatten()(x)
+
+    return tf.keras.Model(represent_input, x)
 
 
 
 
 def GetModel(fbg_count):
     spectra_input = tf.keras.Input((1000,))
-    x = tf.expand_dims(spectra_input, axis=2)
-    x = tf.keras.layers.Conv1D(fbg_count*20, 3, activation='relu')(x)
-    x = tf.keras.layers.MaxPooling1D(2)(x)
-    x = tf.keras.layers.Conv1D(fbg_count*20, 5, activation='relu')(x)
-    x = tf.keras.layers.MaxPooling1D(2)(x)
-    x = tf.keras.layers.Conv1D(fbg_count*20, 5, activation='relu')(x)
-    x = tf.keras.layers.MaxPooling1D(2)(x)
-    x = tf.keras.layers.Flatten()(x)
-    x = tf.keras.layers.Dense(fbg_count*50,activation='sigmoid')(x)
-    x = tf.keras.layers.Dense(fbg_count)(x)
-    return tf.keras.Model(spectra_input, x)
+
+    encoder = Encoder(fbg_count)
+    decoder = Decoder(fbg_count)
+    
+    x = spectra_input
+    
+    encoded = encoder(x)
+    decoded = decoder(encoded)
+
+
+    wl = tf.keras.layers.Dense(fbg_count)(encoded)
+
+    encdec = tf.keras.Model(spectra_input, decoded)
+    wlcnn = tf.keras.Model(spectra_input, wl)
+    return encdec, wlcnn
