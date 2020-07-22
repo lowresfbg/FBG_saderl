27f731f44b87a194625e6608f2450e927094e077
Hans Chiu <chiuhans111@gmail.com>
2020-07-22 17:46:01 +0800

    refine model

--- ML_AutoEncoder.py
+++ ML_AutoEncoder.py
@@ -3,7 +3,7 @@ from Dataset.Simulation.GaussCurve_TF import FBG_spectra
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-fbgs = 10
+fbgs = 5
 samples = 40000
 
 encdec, model = AutoEncoderWLCNN.GetModel(3)
