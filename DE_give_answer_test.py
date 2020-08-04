edc42d5faa8ae0f7ffa7a352194e653a814b7b16
Hans Chiu <chiuhans111@gmail.com>
2020-08-04 18:14:03 +0800

    diff amount of fbgs

--- DE_give_answer_test.py
+++ DE_give_answer_test.py
@@ -30,150 +30,207 @@ cmap = matplotlib.cm.get_cmap('cool')
 print('loading dataset')
 # [:,:,948-76:1269-76]
 
-fbgs = 10
-samples = 1000
+samples = 10
+testRepeat = 10
+ITERATION = 1000
+
+errorOverIterationsLineGroup = []
+fbgsCounts = [3,5,10]
 
 
 
-x_coord = tf.linspace(1545.0, 1549.0, 1000)
+def plot():
 
-X1 = tf.random.uniform([samples, fbgs])*3+1545.5
-I1 = tf.repeat([[ 1, 0.5, 0.3, 0.2, 0.1 ,0.05,0.03,0.02,0.01,0.005 ]], samples, axis=0)*1e3
-W1 = tf.ones([samples, fbgs]) * 0.2
-spectrums1 = FBG_spectra(x_coord, X1, I1, W1) 
-spectrums1 = spectrums1 + (tf.random.uniform(spectrums1.shape)-0.5) * 0.001
+    average = np.zeros([len(fbgsCounts),2,ITERATION])
+    n_of_line = np.zeros([len(fbgsCounts),2,ITERATION])
+    std = np.zeros([len(fbgsCounts),2,ITERATION])
 
-dataset =tf.concat([ tf.repeat(x_coord[tf.newaxis,tf.newaxis, :], samples, axis=0), tf.expand_dims(spectrums1, axis=1) ] , axis=1)
+    def padTo(array, length):
+        return np.concatenate([array, np.zeros(length-array.shape[0])], axis=0)
 
-print(dataset)
-print('loading completed')
+    for (i,errorOverIterationsLine) in enumerate(errorOverIterationsLineGroup):
+        for (j,line) in enumerate(errorOverIterationsLine):
+            ga = j%2
+            average[i,ga]+=padTo(line, ITERATION)
+            n_of_line[i,ga] += padTo(line*0+1, ITERATION)
 
-plt.plot(*dataset[0])
-plt.show()
-ITERATION = 2000
+    average /= n_of_line
 
-de = DE()
+    for (i,errorOverIterationsLine) in enumerate(errorOverIterationsLineGroup):
+        for (j,line) in enumerate(errorOverIterationsLine):
+            ga = j%2
+            std[i,ga] += (padTo(line, ITERATION) - average[i,ga])**2
 
-def InsertAnswer(info, answer):
-    i, data, X = info
-    amount = 25
-    if i<1:
-        de.X = tf.concat([ tf.repeat([answer],amount, axis=0) + (tf.random.uniform([amount, fbgs])-0.5)*2*0.001 , de.X[amount:]], axis=0)
+    std /= n_of_line
+    std = np.sqrt(std)
 
+    plt.xscale('log')
+    plt.yscale('log')
 
-
-def giveAnswerGenerator(answer):
-    return lambda info : InsertAnswer(info, answer)
-
-
-I = I1[0]
-W = W1[0]
-
-def init(de, answer, giveAnswer = False):
-    de.minx = 1545.0
-    de.maxx = 1549.0
-    de.I = I
-    de.W = W
-    de.NP = 50
-    de.CR = 0.75
-    de.F = 0.5
-    # de.Ranged = True
-    de.EarlyStop_threshold = 1e-3
-    # de.spectra_diff = spectra_diff_contrast
-    de.beforeEach = []
-
-    if giveAnswer:
-        de.beforeEach.append(giveAnswerGenerator(answer))
-
-
-
-
-sl = SingleLogger(I, W)
-
-print("start single test")
-init(de, X1[3])
-de.afterEach.append(sl.log)
-de.run(dataset[3], max_iter=ITERATION)
-sl.PlotFigure()
-plt.show()
-print("single test complete")
-
-
-X_log = []
-err_log = []
-
-de.afterEach.clear()
-print(x_coord)
-print(spectrums1[0])
-
-data = tf.concat([
-    [x_coord],
-    [spectrums1[0]]
-], axis=0)
-print(data.shape)
-
-
-input_error_rmse_log = []
-iterations_log = []
-output_error_rmse_log = []
-c = []
-
-errorOverIterationsLine = []
-
-
-for rt in range(10):
-    for i in range(spectrums1.shape[0]):
+    for (i,fbgs) in enumerate(fbgsCounts):
         for j in range(2):
-            sl = SingleLogger(I, W)
-            sl.Animate = False
+            a = average[i,j]
+            d = std[i,j]
+            ga = j%2==0
+            label = "{}fbgs".format(fbgs)
+            if ga:
+                label+="-Ours"
+            p = plt.plot(a, label=label)
+            plt.fill_between(np.arange(ITERATION), a-d, a+d, facecolor=p[0].get_color(), alpha=0.5)
 
 
-            input_error = (tf.random.uniform([fbgs])-0.5)*2*0.001
-            giveAnswer = j%2==0
-
-            input_error_rmse = tf.sqrt(tf.reduce_mean(input_error**2))
-            input_error_rmse_log.append(input_error_rmse)
-
-            data = tf.concat([
-                [x_coord],
-                [spectrums1[i]]
-            ], axis=0)
-
-            print('run ------------------')
-
-            answer = X1[i]
-            init(de, answer+input_error, giveAnswer)
-            de.afterEach = [sl.log]
-
-            X = de(data, max_iter=ITERATION)
-
-            x_mean = tf.reduce_mean(X, axis=0)
-
-            iterations_log.append(de.iter)
-            output_error_rmse = tf.sqrt(tf.reduce_mean((x_mean-answer)**2))
-            output_error_rmse_log.append(output_error_rmse)
-
-            errorOverIterationsLine.append(np.sqrt(np.mean((np.array(sl.X_mean_log)-np.array(answer)[np.newaxis, :])**2,axis=1)))
+    plt.legend()
             
 
-            if giveAnswer:
-                c.append(cmap(input_error_rmse*1000))
-            else:
-                c.append((1,.6,.1))
+for fbgs in fbgsCounts:
 
-            plt.clf()
+    x_coord = tf.linspace(1545.5, 1548.5, 1000)
 
-            plt.xscale('log')
-            plt.yscale('log')
+    X1 = tf.random.uniform([samples, fbgs])*3+1545.5
+    I1 = tf.repeat([[ 1, 0.5, 0.3, 0.2, 0.1 ,0.05,0.03,0.02,0.01,0.005 ][:fbgs]], samples, axis=0)*1e3
+    W1 = tf.ones([samples, fbgs]) * 0.2
+    
+    spectrums1 = FBG_spectra(x_coord, X1, I1, W1) 
+    spectrums1 = spectrums1 + (tf.random.uniform(spectrums1.shape)-0.5) * 0.0001
 
-            plt.scatter(np.array(iterations_log),np.array(output_error_rmse_log), c=c, s=4)
-
-            for j,line in enumerate(errorOverIterationsLine):
-                plt.plot(line, c=c[j], alpha=0.1)
+    dataset =tf.concat([ tf.repeat(x_coord[tf.newaxis,tf.newaxis, :], samples, axis=0), tf.expand_dims(spectrums1, axis=1) ] , axis=1)
 
 
-            plt.pause(0.01)
-            plt.tight_layout()
+    print(dataset)
+    print('loading completed')
 
+    # plt.plot(*dataset[0])
+    # plt.show()
+    
+
+    de = DE()
+
+    def InsertAnswer(info, answer):
+        i, data, X = info
+        amount = 50
+        if i<1:
+            de.X = tf.concat([ tf.repeat([answer],amount, axis=0) + (tf.random.uniform([amount, fbgs])-0.5)*2*0.002 , de.X[amount:]], axis=0)
+
+
+
+    def giveAnswerGenerator(answer):
+        return lambda info : InsertAnswer(info, answer)
+
+
+    I = I1[0]
+    W = W1[0]
+
+    def init(de, answer, giveAnswer = False):
+        de.minx = 1545.0
+        de.maxx = 1549.0
+        de.I = I
+        de.W = W
+        de.NP = 100
+        de.CR = 0.75
+        de.F = 0.5
+        de.Ranged = True
+        de.EarlyStop_threshold = 1e-3
+        # de.spectra_diff = spectra_diff_contrast
+        de.beforeEach = []
+
+        if giveAnswer:
+            de.beforeEach.append(giveAnswerGenerator(answer))
+
+
+
+
+    # sl = SingleLogger(I, W)
+
+    # print("start single test")
+    # init(de, X1[3])
+    # de.afterEach.append(sl.log)
+    # de.run(dataset[3], max_iter=ITERATION)
+    # sl.PlotFigure()
+    # plt.show()
+    # print("single test complete")
+
+
+    # X_log = []
+    # err_log = []
+
+    # de.afterEach.clear()
+    # print(x_coord)
+    # print(spectrums1[0])
+
+    # data = tf.concat([
+    #     [x_coord],
+    #     [spectrums1[0]]
+    # ], axis=0)
+    # print(data.shape)
+
+
+    # input_error_rmse_log = []
+    # iterations_log = []
+    # output_error_rmse_log = []
+    # c = []
+
+    errorOverIterationsLine = []
+
+
+    for rt in range(testRepeat):
+        for i in range(spectrums1.shape[0]):
+            for j in range(2):
+                sl = SingleLogger(I, W)
+                sl.Animate = False
+
+
+                input_error = (tf.random.uniform([fbgs])-0.5)*2*0.001
+                giveAnswer = j%2==0
+
+                # input_error_rmse = tf.sqrt(tf.reduce_mean(input_error**2))
+                # input_error_rmse_log.append(input_error_rmse)
+
+                data = tf.concat([
+                    [x_coord],
+                    [spectrums1[i]]
+                ], axis=0)
+
+                print('run ------------------')
+
+                answer = X1[i]
+                init(de, answer+input_error, giveAnswer)
+                de.afterEach = [sl.log]
+
+                X = de(data, max_iter=ITERATION)
+
+                # x_mean = tf.reduce_mean(X, axis=0)
+
+                # iterations_log.append(de.iter)
+                # output_error_rmse = tf.sqrt(tf.reduce_mean((x_mean-answer)**2))
+                # output_error_rmse_log.append(output_error_rmse)
+
+                errorOverIterationsLine.append(np.sqrt(np.mean((np.array(sl.X_mean_log)-np.array(answer)[np.newaxis, :])**2,axis=1)))
+                
+
+                # if giveAnswer:
+                #     c.append(cmap(input_error_rmse*1000))
+                # else:
+                #     c.append((1,.6,.1))
+
+                # plt.clf()
+
+                # plt.xscale('log')
+                # plt.yscale('log')
+
+                # plt.scatter(np.array(iterations_log),np.array(output_error_rmse_log), c=c, s=4)
+
+                # for j,line in enumerate(errorOverIterationsLine):
+                #     plt.plot(line, alpha=0.1)
+
+
+                # plt.pause(0.01)
+                # plt.tight_layout()
+
+    errorOverIterationsLineGroup.append(errorOverIterationsLine)
+    plt.clf()
+    plot()
+    plt.pause(0.01)
 
 plt.show()
+
+
