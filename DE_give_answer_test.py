b4ab2e4c488c24a62d485896d612bc8bfd504739
Hans Chiu <chiuhans111@gmail.com>
2020-08-26 11:45:56 +0800

    redo image

--- DE_give_answer_test.py
+++ DE_give_answer_test.py
@@ -98,8 +98,8 @@ def plot():
     plt.xscale('log')
     plt.yscale('log')
 
-    for (i,fbgs) in enumerate(fbgsCounts):
-        for j in range(2):
+    for j in range(2):
+        for (i,fbgs) in enumerate(fbgsCounts):
             a = average[i,j]
             d = std[i,j]
             ga = j%2==1
@@ -110,7 +110,7 @@ def plot():
             plt.fill_between(np.arange(ITERATION), a-d, a+d, facecolor=p[0].get_color(), alpha=0.5)
 
 
-    plt.legend(fontsize=6, ncol=3)
+    plt.legend(fontsize=6, ncol=2, loc='lower left')
             
 plotAll()
 
@@ -268,7 +268,7 @@ def calculate():
         plt.pause(0.01)
     print('done')
 
-calculate()
+# calculate()
 plt.show()
 
 
